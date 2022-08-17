d5e4396f6d834ad08023f4d04824abffde5f6223
Ercan ALTUN <salsel0147@gmail.com>
2022-08-17 16:23:34 +0300

    m5

--- src/Gitignore.java
+++ src/Gitignore.java
@@ -5,7 +5,8 @@ public class Gitignore {
         System.out.println("Git ignore deneme 2.");
         System.out.println("Git ignore deneme 3.");
         System.out.println("Git ignore deneme 4.");
-        // Not:Branch ercan'dayken Pull yapabilecek miyiz?
+         // Not:Branch ercan'dayken Pull yapabilecek miyiz?
+        // Not2: Pull yapabilmek icin local'imizdeki branch'in master olamsi gerekir
 
     }
 }
